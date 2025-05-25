7c372162ec1992a767e4b68c3af7d2d02f00505e
luis-pessoa <lgustavonunes@gmail.com>
2025-05-25 19:13:17 -0300

    Sua mensagem de commit aqui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
+import Header from './Header'
+import '../index.css'
+
 function App() {
 
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className="flex justify-between items-center bg-[var(--gray-7) h-[80px]]">
+      <div>
+        <img src="./Logo.png" alt="" />
+      </div>
+
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
